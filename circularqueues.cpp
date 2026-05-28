557d4077a3e413562e3803685084ef1df3cc5e91
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-28 10:25:29 +0700

    penempatan logika pertama

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -28,3 +28,10 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+
+    // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
